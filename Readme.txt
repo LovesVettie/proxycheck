27d180436f1a1e9dfe7146e9ff450464bc58bf3c
H04x <101000391+LovesVettie@users.noreply.github.com>
2023-04-16 17:48:19 +0300

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,17 @@
+Bu yöntem İle bulunan Proxilerin Kullanımıyla herhangi bir ilgim yoktur.
+Bütün Telif hakları Şahsım Olan H04x'a Aittir!
+-----------------------------------------------
+    TR           Kullanım
+----------------          ---------------------
+- Proxy.txt Adındaki Dosyanın içine Check Edilecek Proxy'leri yazın.
+- Daha Sonra Klasör içinde 'CMD' Açın Ve > "pip install -r requirements.txt" Yazıp Enter Basın.
+- Şimdi ise Tekrar 'CMD' Açıp içine > "python h04xproxies.py" yazıyoruz,
+- Ve Karşınıza Dosya Girme Yeri çıkıcak Oraya ise 'Proxy.txt' Yazıp Bekliyoruz Ve Check Etmeye Başlıyor.
+
+-----------------------------------------------
+    EN            Usage
+-----------------      ------------------------        
+- Write Proxies to Check in the File Named Proxy.txt.
+- Then Open 'CMD' in the folder and > Type "pip install -r requirements.txt" and press Enter.
+- Now open 'CMD' again and write > "python h04xproxies.py" in it,
+- And the File Entering Place will appear. We write 'Proxy.txt' and wait and it starts to check.
---
+++ h04xproxies.py
@@ -0,0 +1,54 @@
+import os,  time, requests, sys, threading
+PyVer = str(sys.version)
+
+def cls():
+    linux = 'clear'
+    windows = 'cls'
+    os.system([linux,windows][os.name == 'nt'])
+cls()
+
+def xx(PROXY, url):
+    try:
+        s = requests.sion()
+        s.proxies = {'http': PROXY}
+        s.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                                      ' (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+        aa = s.get(url, timeout=5, proxies={'http': PROXY})
+        if aa.status_code == 200:
+            print (PROXY + '        ÇALISIYOR')
+            with open('proxys.txt', 'a') as xX:
+                xX.write(PROXY + '\n')
+        else:
+            print (PROXY + '          Kötü')
+    except:
+        print (PROXY + '          Kötü')
+def main():
+    try:
+        if '3.' in PyVer:
+            try:
+                fileproxy = input('PROXY DOSYASI GIR  root/kali$ ')
+            except:
+                print('  HATA! : Proxy Gir!')
+                sys.exit()
+        elif '2.' in PyVer:
+            try:
+                fileproxy = raw_input('PROXY DOSYASI GIR  root/kali$ ')
+            except:
+                print('  HATA! : Proxy Gir!')
+                sys.exit()
+        else:
+            print(' Unknown Python Version!')
+    except:
+        pass
+        sys.exit()
+    with open(fileproxy, 'r') as x:
+        prox = x.read().splitlines()
+    thread = []
+    for proxy in prox:
+        t = threading.Thread(target=xx, args=(proxy, 'https://instagram.com'))
+        t.start()
+        thread.append(t)
+        time.sleep(0.1)
+    for i in thread:
+        i.join()
+main()
